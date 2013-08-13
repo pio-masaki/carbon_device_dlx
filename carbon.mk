73c219bfb14ef034254f9e747b31478aeb313349
pio_masaki <piomasaki@gmail.com>
2013-08-13 14:58:02 -0700

    Oops, this is the CDMA branch

--- carbon.mk
+++ carbon.mk
@@ -1,5 +1,5 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/carbon/config/common_gsm.mk)
+$(call inherit-product, vendor/carbon/config/common_cdma.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
